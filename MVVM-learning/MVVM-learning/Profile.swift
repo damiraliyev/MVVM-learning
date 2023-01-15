58d3e23201909a3171ea9b5325e05f9b2cf4b8a4
damiraliyev <damir.aliyev.02@mail.ru>
2023-01-15 18:29:09 +0600

    create Profile model

---
+++ MVVM-learning/MVVM-learning/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-learning
+//
+//  Created by Damir Aliyev on 15.01.2023.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
